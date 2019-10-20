e721669d976a87a2a13953a2545d92b09f53d82f
Frank Tiersch <mail@ftiersch.de>
2019-10-20 13:00:15 +0200

    Change User password

--- src/Services/UserService.php
+++ src/Services/UserService.php
@@ -134,6 +134,34 @@ class UserService {
         }
     }
 
+    public function changePassword($userId, $newPassword) {
+        try {
+
+            $response = $this->client->request('PUT', 'user/' . $userId . '/password', [
+                'json'      => ['password' => $newPassword],
+                'headers'   => [
+                    'Accept'    => 'application/json',
+                ],
+            ]);
+
+            if (in_array($response->getStatusCode(), [200, 201])) {
+                return true;
+            }
+
+            return false;
+        } catch (RequestException $e) {
+            if ($e->hasResponse()) {
+                $response = $e->getResponse();
+
+                if ($response->getStatusCode() === 422) {
+                    throw ValidationException::withMessages(json_decode($response->getBody(), true)['errors']);
+                }
+
+                throw new UpdateUserException("Error during update of email:" . $response->getStatusCode() . " - " . (string)$response->getBody());
+            }
+        }
+    }
+
     public function updateUser($id) {
         return new UserUpdater($id);
     }
