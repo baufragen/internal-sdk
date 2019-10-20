c4a1fd9ba450af07009c8145f4e9983011bdcbdd
Frank Tiersch <mail@ftiersch.de>
2019-10-20 12:55:06 +0200

    Change User email

--- src/Services/UserService.php
+++ src/Services/UserService.php
@@ -10,13 +10,17 @@ use \GuzzleHttp\Exception\RequestException;
 use \Illuminate\Validation\ValidationException;
 
 class UserService {
-    public function registerUser($email, $password, $origin, $additional = []) {
-        /** @var BaufragenClient $client */
-        $client = app(BaufragenClient::class);
+    /** @var BaufragenClient $client */
+    protected $client;
 
+    public function __construct() {
+        $this->client = app(BaufragenClient::class);
+    }
+
+    public function registerUser($email, $password, $origin, $additional = []) {
         try {
 
-            $response = $client->request('POST', 'auth/register', [
+            $response = $this->client->request('POST', 'auth/register', [
                 'form_params' => array_merge([
                     'email'     => $email,
                     'password'  => $password,
@@ -47,12 +51,9 @@ class UserService {
     }
 
     public function setAvatar($userId, $avatar) {
-        /** @var BaufragenClient $client */
-        $client = app(BaufragenClient::class);
-
         try {
 
-            $response = $client->request('POST', 'user/' . $userId . '/avatar', [
+            $response = $this->client->request('POST', 'user/' . $userId . '/avatar', [
                 'multipart' => [
                     [
                         'name'     => 'avatar',
@@ -82,12 +83,9 @@ class UserService {
     }
 
     public function deleteAvatar($userId) {
-        /** @var BaufragenClient $client */
-        $client = app(BaufragenClient::class);
-
         try {
 
-            $response = $client->request('DELETE', 'user/' . $userId . '/avatar');
+            $response = $this->client->request('DELETE', 'user/' . $userId . '/avatar');
 
             if (in_array($response->getStatusCode(), [200, 201])) {
                 return true;
@@ -108,17 +106,42 @@ class UserService {
         }
     }
 
+    public function changeEmail($userId, $newEmail) {
+        try {
+
+            $response = $this->client->request('PUT', 'user/' . $userId . '/email', [
+                'json'      => ['email' => $newEmail],
+                'headers'   => [
+                    'Accept'    => 'application/json',
+                ],
+            ]);
+
+            if (in_array($response->getStatusCode(), [200, 201])) {
+                return true;
+            }
+
+            return false;
+        } catch (RequestException $e) {
+            if ($e->hasResponse()) {
+                $response = $e->getResponse();
+
+                if ($response->getStatusCode() === 422) {
+                    throw ValidationException::withMessages(json_decode($response->getBody(), true)['errors']);
+                }
+
+                throw new UpdateUserException("Error during update of email:" . $response->getStatusCode() . " - " . (string)$response->getBody());
+            }
+        }
+    }
+
     public function updateUser($id) {
         return new UserUpdater($id);
     }
 
     public function executeUpdateUser(UserUpdater $userUpdater) {
-        /** @var BaufragenClient $client */
-        $client = app(BaufragenClient::class);
-
         try {
 
-            $response = $client->request('PUT', 'user/' . $userUpdater->getUser(), [
+            $response = $this->client->request('PUT', 'user/' . $userUpdater->getUser(), [
                 'json'      => $userUpdater->getData(),
                 'headers'   => [
                     'Accept'    => 'application/json',
