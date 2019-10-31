658c3c20bf03f860d41b2c1f192861ace08ff802
Frank Tiersch <mail@ftiersch.de>
2019-10-31 07:03:13 +0100

    Add apikey security

--- src/Middleware/SdkRequestsOnly.php
+++ src/Middleware/SdkRequestsOnly.php
@@ -8,7 +8,10 @@ class SdkRequestsOnly
 {
     public function handle($request, Closure $next)
     {
-        if (!$request->header('baufragen_sdk_apikey') || $request->header('baufragen_sdk_apikey', null) != config('baufragensdk.apikey')) {
+        if (!$request->header('baufragen_sdk_apikey', null)) {
+            abort(400, "No ApiKey");
+        }
+        if ($request->header('baufragen_sdk_apikey', null) != config('baufragensdk.apikey')) {
             abort(403, "Wrong ApiKey");
         }
 
